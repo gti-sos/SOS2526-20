653fd13593809fe6f8e0e256cc9581ab31c45fb7
Liu11037 <amaalcper@alum.us.es>
2026-02-28 13:37:04 +0100

    samplesAAParreglo3

--- index.js
+++ index.js
@@ -20,10 +20,8 @@ app.listen(PORT, () => {
 })
 
 
-// app.use("/samples/AAP", express.static("./samples/AAP/index.js")); //Devuelve todo el codigo
 
 let AAP = require("./samples/AAP/index.js");
-const devuelveMedia = require("./samples/AAP/index.js");
 
 app.get('/samples/AAP', (req, res) => {
   res.send(`<html><body><h1>
