200e61b9e5be24af269edbc840c90cdf0186d84a
Liu11037 <amaalcper@alum.us.es>
2026-03-01 12:58:43 +0100

    baseurl

--- index.js
+++ index.js
@@ -28,3 +28,16 @@ app.get('/samples/AAP', (req, res) => {
             ${AAP()}
             </h1></body></html>`);
 });
+
+
+
+let BASE_URL_API = "/api/v1";
+
+let picantes = [
+  { domain_code: "TCL", domain: "Crops and livestock products", area_code_m49: 4, area: "Afghanistan", element_code: 5610, item_code_cpc: 1654, item: "Anise, badian, coriander, cumin, caraway, fennel and juniper berries, raw", year: 2014, unit: "t", import: 283.85, export: 21099, production: 21500, consumption: 684.85 },
+  { domain_code: "TCL", domain: "Crops and livestock products", area_code_m49: 4, area: "Afghanistan", element_code: 5610, item_code_cpc: 1654, item: "Anise, badian, coriander, cumin, caraway, fennel and juniper berries, raw", year: 2015, unit: "t", import: 1000.16, export: 17340, production: 18000, consumption: 1660.16 }
+];
+
+app.get(BASE_URL_API+"/spice-stats", (req, res) =>{
+  res.send(JSON.stringify(picantes));
+});
