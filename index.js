e1984730416ec8a639251af7b3cea328cb753138
Liu11037 <amaalcper@alum.us.es>
2026-03-10 13:11:50 +0100

    carpeta html

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 // ============================================================================
 
 app.use("/about",express.static("./README.md"));
-app.use("/", express.static("./onrendercom.html"));
+app.use("/", express.static("./html"));
 app.use(bodyParser.json());
 
 
