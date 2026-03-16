efc74fcadf07c82fe964a91172839dc075c66d4a
Liu11037 <amaalcper@alum.us.es>
2026-03-16 16:43:33 +0100

    quitar comentarios

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// let cool = require("cool-ascii-faces");
-// let express = require("express");
-// let bodyParser = require('body-parser');
-
-// import cool from 'cool-ascii-faces';
 import express from 'express';
 import bodyParser from 'body-parser';
 
@@ -10,7 +5,6 @@ import { loadBackendAAP } from './src/back/indexAAP.js';
 import { loadBackendPMG } from './src/back/indexPMG.js';
 import { loadBackendFJGM } from './src/back/indexFJGM.js';
 
-let BASE_URL_API = "/api/v1";
 let PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -18,12 +12,8 @@ const app = express();
 // ============================================================================
 // ============================================================================
 
-// app.use("/", express.static("./onrendercom.html"));
 app.use("/about", express.static("./static/about.html"));
 app.use("/", express.static("./static"));
-
-// const path = require("path");
-import path from 'path';
 
 app.use(bodyParser.json());
 
@@ -32,7 +22,6 @@ app.use(bodyParser.json());
 loadBackendAAP(app);
 loadBackendPMG(app);
 loadBackendFJGM(app);
-
 
 
 
