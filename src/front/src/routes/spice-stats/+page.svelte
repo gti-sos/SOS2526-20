ee91e935f0c287d25197b7c2270623b131eb160a
Liu11037 <amaalcper@alum.us.es>
2026-03-20 18:27:18 +0100

    spice

---
+++ src/front/src/routes/spice-stats/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    // @ts-ignore
+    let spices = $state([]);
+
+    async function getspices(){
+        const res = await fetch("http://localhost:3000/api/v1/spice-stats/", {
+            method: "GET"
+        });
+        const data = await res.json();
+        spices = data;
+    }
+</script>
+
+<p>Spices</p>
+
+{#each spices as spice (`${spice.area}-${spice.item}-${spice.year}`)}
+    <tr>
+        <td>{spice.area}</td>
+        <td>{spice.item}</td>
+        <td>{spice.year}</td>
+    </tr>
+{/each}
+
+
+<button onclick={getspices}>Refresh</button>
