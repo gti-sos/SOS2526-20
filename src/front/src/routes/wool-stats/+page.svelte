c3e77feaab24c2ac0dd1107625d5080b5478eb76
GTT7265 <javichulo2004@gmail.com>
2026-03-20 17:16:52 +0100

    page svelte wool

---
+++ src/front/src/routes/wool-stats/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+   let wools = $state([])
+
+async function getwool(){
+   const res = await fetch("http://localhost:3000/api/v1/wool-stats",{
+        method: "GET"
+   });
+   const data = await res.json();
+   wools = data;
+}
+</script>
+
+<p>LANA</p>
+<ul>
+{#each  wools as wool (wool.period, wool.reporterDesc,wool.flowDesc)}
+    <li> {wool.period} - {wool.reporterDesc} - {wool.flowDesc}</li>
+{/each}
+</ul>
+
+<button onclick={getwool}>Refresh</button>
