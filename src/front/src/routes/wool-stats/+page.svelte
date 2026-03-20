2c4c314b5a518ffacb877c8a984fad76c250a14c
GTT7265 <javichulo2004@gmail.com>
2026-03-20 18:28:35 +0100

    nada nada

--- src/front/src/routes/wool-stats/+page.svelte
+++ src/front/src/routes/wool-stats/+page.svelte
@@ -1,25 +1,29 @@
 <script>
+  // @ts-ignore
   let wools = $state([]);
   import { dev } from '$app/environment';
 
   let API = '/api/v1/wool-stats';
 
-  if (dev)
-    API = "http://localhost:3000" + API;
+  if(dev)
+    API = "http://localhost:3000"+API;
 
-  async function getWool() {
-    const res = await fetch(API);
-    const data = await res.json();
-    wools = data;
-  }
+async function getWool(){
+  const res = await fetch(API,{
+    method : "GET"
+  });
+  const data = await res.json();
+  wools  = data;
+}
+
 </script>
 
 <p>Wool</p>
 
 <ul>
-  {#each wools as wool}
-    <li>{wool.period} - {wool.reporterDesc} - {wool.flowDesc}</li>
-  {/each}
+{#each wools as wool (wool.period, wool.reporterDesc,wool.flowDesc)}
+  <li>{wool.period} - {wool.reporterDesc} - {wool.flowDesc}</li>
+{/each}
 </ul>
 
 <button onclick={getWool}>Refresh</button>
