8d5909ca2af60222565f9813d3848a271ee50b53
Liu11037 <amaalcper@alum.us.es>
2026-03-20 17:06:12 +0100

    +page

--- src/front/src/routes/Header.svelte
+++ src/front/src/routes/Header.svelte
@@ -1,8 +1,9 @@
 <h2>SOS2526-20</h2>
 
 <nav>
-    <a href="/">Home</a>
-    <!-- <a href="/wool-stats">Lana</a>
+    <!-- <a href="/">Home</a>
+    <a href="/wool-stats">Lana</a>
     <a href="/coffee-stats">Café</a>
-    <a href="/spice-stats">Picante</a> -->
+    <a href="/spice-stats">Picante</a>
+    <a href="/about">About</a> -->
 </nav>
