4dc7a1144b2cd467dbbe5b65d824d61f91490dad
Liu11037 <amaalcper@alum.us.es>
2026-03-20 17:14:03 +0100

    +page

--- src/front/src/routes/+page.svelte
+++ src/front/src/routes/+page.svelte
@@ -10,14 +10,17 @@
         <div>
             <p>Francisco Javier González Macías</p>
             <p><a href="/wool-stats">Api lana</a></p>
+            <p><a href="https://documenter.getpostman.com/view/52409546/2sBXigLYrv">Documentación en Postman</a></p>
         </div>
         <div>
             <p>Pedro Mata García</p>
             <p><a href="/coffee-stats">Api café</a></p>
+            <p><a href="https://documenter.getpostman.com/view/52409546/2sBXigLYrv">Documentación en Postman de Café</a></p>
         </div>
         <div>
             <p>Amanda Liu Alcántara Pérez</p>
             <p><a href="/spice-stats">Api picantes</a></p>
+            <p><a href="https://documenter.getpostman.com/view/52408352/2sBXierDwv"></a>Documentación en Postman</p>
         </div>
     </div>
 </div>
