e4b8cdca78e7d5f4f1b56a1840761a4837b78eb0
GTT7265 <javichulo2004@gmail.com>
2026-03-20 20:15:59 +0100

    co

--- src/front/src/routes/wool-stats/+page.svelte
+++ src/front/src/routes/wool-stats/+page.svelte
@@ -1,35 +1,55 @@
-<script>
-  // @ts-ignore
-  let wools = $state([]);
-  import { dev } from '$app/environment';
 
-  let API = '/api/v1/wool-stats';
-
-  if(dev)
-    API = "http://localhost:3000"+API;
-
-async function getWool(){
-  const res = await fetch(API);
-  const data = await res.json();
-  wools  = data.data;
-}
-
-</script>
-
-<p>Wool</p>
-{wools}
 <!-- <ul>
 {#each wools as wool (wool.period)}
   <li>{wool.period} - {wool.reporterDesc} - {wool.flowDesc}</li>
 {/each}
 </ul> -->
 
-<ul>
-    {#each wools as wool }
-        <li>{wool}</li>
-    {:else}
-        <li>No hay datos disponibles. Pulsa Refresh.</li>
-    {/each}
-</ul>
+<script>
+    // @ts-ignore
+    let wools = $state([]);
+    import { dev } from '$app/environment';
 
-<button onclick={getWool}>Refresh</button>
+    let API = '/api/v1/wool-stats';
+     let resultStatusCode = $state(0);
+    if(dev)
+        API = 'http://localhost:3000'+API;
+
+    async function getWools(){
+        try{
+            const res = await fetch(API);
+            const data = await res.json();
+            wools = data.data;
+        } catch(err){
+            return err;
+        }
+    }
+
+    async function deleteWools(){
+
+    //console.log("DELETE "+name);
+
+    const res = await fetch(API,{
+      method : "DELETE"
+    });
+    resultStatusCode = await res.status;
+    
+    if(resultStatusCode == 200)
+      getWools();
+
+  }
+</script>
+
+<h2>Wools</h2>
+
+{#each wools as wool (`${wool.period}-${wool.reporterDesc}-${wool.flowDesc}`)}
+    <tr>
+        <td>{wool.period}</td>
+        <td>{wool.reporterDesc}</td>
+        <td>{wool.flowDesc}</td>
+    </tr>
+{/each}
+
+
+<button onclick={getWools}>Refresh</button>
+<button onclick={deleteWools}>Borrar</button>
