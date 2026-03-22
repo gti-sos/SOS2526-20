57e36d1d8729f313d498e383db3ad975bfc36c17
Liu11037 <amaalcper@alum.us.es>
2026-03-22 18:05:45 +0100

    mas css

--- src/front/src/routes/+page.svelte
+++ src/front/src/routes/+page.svelte
@@ -3,22 +3,22 @@
         <div class="alum">
             <h4>Francisco Javier González Macías</h4>
             <p>Fuente: wool-stats</p>
-            <p><a href="/wool-stats">Front-End</a></p>
+            <p><a href="/wool-stats">Página web sobre lana</a></p>
             <p><a href="https://sos2526-20.onrender.com/api/v1/wool-stats/">URL base de la API de lana</a></p>
             <p><a href="https://documenter.getpostman.com/view/52409546/2sBXigLYrv">Documentación en Postman</a></p>
         </div>
         <div class="alum">
             <h4>Pedro Mata García</h4>
             <p>Fuente: coffee-stats</p>
-            <p><a href="/coffee-stats">Front-End</a></p>
+            <p><a href="/coffee-stats">Página web sobre café</a></p>
             <p><a href="https://sos2526-20.onrender.com/api/v1/coffee-stats/">URL base de la API de café</a></p>
             <p><a href="https://documenter.getpostman.com/view/52409546/2sBXigLYrv">Documentación en Postman de Café</a></p>
         </div>
         <div class="alum">
             <h4>Amanda Liu Alcántara Pérez</h4>
             <p>Fuente: spice-stats</p>
-            <p><a href="/spice-stats">Front-End</a></p>
-            <p><a href="https://sos2526-20.onrender.com/api/v1/spice-stats/">URL base de la API de picante</a></p>
+            <p><a href="/spice-stats">Página web sobre especias</a></p>
+            <p><a href="https://sos2526-20.onrender.com/api/v1/spice-stats/">URL base de la API de especias</a></p>
             <p><a href="https://documenter.getpostman.com/view/52408352/2sBXierDwv">Documentación en Postman</a></p>
         </div>
     </div>
