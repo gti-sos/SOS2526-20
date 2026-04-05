3d6c5c83ba811543e11eb9c8fb3e310543720ded
Liu11037 <amaalcper@alum.us.es>
2026-04-05 18:52:54 +0200

    todo comentado

--- tests/e2e/index.spec.js
+++ tests/e2e/index.spec.js
@@ -1,23 +1,23 @@
-// @ts-check
-import { test, expect } from '@playwright/test';
+// // @ts-check
+// import { test, expect } from '@playwright/test';
 
-const URL_BASE = process.env.BASE_URL || 'http://localhost:3000';
-const app = `${URL_BASE}/`;
+// const URL_BASE = process.env.BASE_URL || 'http://localhost:3000';
+// const app = `${URL_BASE}/`;
 
-test('has title', async ({ page }) => {
-  await page.goto(app);
+// test('has title', async ({ page }) => {
+//   await page.goto(app);
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/SOS2526-20/);
-});
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/SOS2526-20/);
+// });
 
 
 
-test('spices has title', async ({ page }) => {
-  await page.goto(app);
+// test('spices has title', async ({ page }) => {
+//   await page.goto(app);
 
-  await page.getByRole('link', { name: 'Página web sobre especias' }).click();
+//   await page.getByRole('link', { name: 'Página web sobre especias' }).click();
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Picantes/);
-});
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Picantes/);
+// });
