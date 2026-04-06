f12ee1b44d2c7d81c4f5c4742a9665f54de8c6f0
GTT7265 <javichulo2004@gmail.com>
2026-04-06 16:41:43 +0200

    v1

--- src/back/v2/indexFJGM.js
+++ src/back/v2/indexFJGM.js
@@ -7,7 +7,7 @@ let db = new dataStore();       //Variable con la base de datos
 function loadBackendFJGM(app) {
 
     app.get('/api/v1/wool-stats/docs', (req, res) => {
-    res.redirect('https://documenter.getpostman.com/view/52409546/2sBXigLYrv');
+    res.redirect('https://documenter.getpostman.com/view/52408471/2sBXigLYrw');
     });
     
     app.get('/api/v2/wool-stats/docs', (req, res) => {
