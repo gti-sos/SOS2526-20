ea4baf38c2e9423208a28e34d4c55c685c0f3a3b
Liu11037 <amaalcper@alum.us.es>
2026-05-04 15:22:09 +0200

    aap inicio

--- src/front/src/routes/integrations/+page.svelte
+++ src/front/src/routes/integrations/+page.svelte
@@ -19,11 +19,11 @@
        </div>
         <div class="alum">
           <h4>Amanda Liu Alcántara Pérez</h4>
-          <p><a href="##">Integracion</a></p>
-          <p><a href="##">Integracion</a></p>
-          <p><a href="##">Integracion</a></p>
-          <p><a href="##">Integracion</a></p>
-          <p><a href="##">Integracion</a></p> 
+          <p><a href="/g11-road-fatalities">g11-road-fatalities</a></p>
+          <p><a href="/g12-birth-death-growth-rates">g12-birth-death-growth-rates</a></p>
+          <p><a href="/g17-water-productivities">g17-water-productivities</a></p>
+          <p><a href="/g21-cholera-stats">g21-cholera-stats</a></p>
+          <p><a href="/g26-countries-idh-per-year">g26-countries-idh-per-year</a></p> 
         </div>
     </div>
 </div>
