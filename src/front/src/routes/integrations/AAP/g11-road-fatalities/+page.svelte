6b6f042bf2d593a2390e2720880bcf67d79ca98b
Liu11037 <amaalcper@alum.us.es>
2026-05-13 20:36:46 +0200

    highcharts more

--- src/front/src/routes/integrations/AAP/g11-road-fatalities/+page.svelte
+++ src/front/src/routes/integrations/AAP/g11-road-fatalities/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import { onMount } from "svelte";
-    import Highcharts from "highcharts";    
+    import Highcharts from "highcharts";
+    import HighchartsMore from "highcharts/highcharts-more";
 
     onMount(async () => {
 
